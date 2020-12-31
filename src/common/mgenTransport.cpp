058890b4079897564fef8db4f1356ae2f5e07de7
sdshlanta <sdshlanta@gmail.com>
2020-12-31 04:57:18 -0500

    fdas

--- src/common/mgenTransport.cpp
+++ src/common/mgenTransport.cpp
@@ -327,9 +327,6 @@ void MgenTransport::RemoveFromPendingList()
 
 void MgenTransport::LogEvent(LogEventType eventType, MgenMsg* theMsg, const struct timeval& theTime, UINT32* buffer)
 {
-    FILE* fp = fopen("timesLogEvent", "a");
-    struct timespec tstart={0,0}, tend={0,0};
-    clock_gettime(CLOCK_MONOTONIC, &tstart);
     if (!(mgen.GetLogFile()))
       return;  
 
@@ -481,15 +478,6 @@ void MgenTransport::LogEvent(LogEventType eventType, MgenMsg* theMsg, const stru
       DMSG(0,"MgenTransport::LogEvent() Error: Invalid LogEvent type.\n");
       break;
     }
-    clock_gettime(CLOCK_MONOTONIC, &tend);
-    fprintf(fp,
-        "Doing the UpdateRecvAnalytics took about %.5f seconds\n",
-        ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
-        ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
-    fflush(fp);
-    bzero(&tstart, sizeof(tstart));
-    bzero(&tend, sizeof(tend));
-    fclose(fp);
 
 } // End MgenTransport::LogEvent()
 
@@ -996,6 +984,10 @@ void MgenUdpTransport::OnEvent(ProtoSocket& theSocket, ProtoSocket::Event theEve
                         {
                             clock_gettime(CLOCK_MONOTONIC, &tstart);
                             ProcessRecvMessage(theMsg, ProtoTime(currentTime));
+                            if (mgen.ComputeAnalytics())
+                                mgen.UpdateRecvAnalytics(currentTime, &theMsg, UDP);
+                            if (mgen.GetLogFile())
+                                LogEvent(RECV_EVENT, &theMsg, currentTime, alignedBuffer);
                             clock_gettime(CLOCK_MONOTONIC, &tend);
                             fprintf(fp,
                                 "Doing the ProcessRecvMessage took about %.5f seconds\n",
@@ -1004,10 +996,6 @@ void MgenUdpTransport::OnEvent(ProtoSocket& theSocket, ProtoSocket::Event theEve
                             fflush(fp);
                             bzero(&tstart, sizeof(tstart));
                             bzero(&tend, sizeof(tend));
-                            if (mgen.ComputeAnalytics())
-                                mgen.UpdateRecvAnalytics(currentTime, &theMsg, UDP);
-                            if (mgen.GetLogFile())
-                                LogEvent(RECV_EVENT, &theMsg, currentTime, alignedBuffer);
                         }
                     }
                     else 
